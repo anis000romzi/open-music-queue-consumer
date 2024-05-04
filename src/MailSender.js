493bdb716be781babb6d791c75bf0996c81aa03d
Anis Romzi <anis000romzi@gmail.com>
2024-05-04 11:20:32 +0700

    mailer

--- src/MailSender.js
+++ src/MailSender.js
@@ -5,6 +5,7 @@ class MailSender {
     this._transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
+      secure: true,
       auth: {
         user: process.env.MAIL_ADDRESS,
         pass: process.env.MAIL_PASSWORD,
